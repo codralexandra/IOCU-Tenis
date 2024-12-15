d2a16f5de3816c8bdf051881fa3934bf5be9db52
Alexandra Codreanu <ioana.codreanu@student.unitbv.ro>
2024-12-15 23:19:27 +0200

    Added ShotManager to Bot + 3 botTargets

--- Tenis3D/Assets/Scripts/Bot.cs
+++ Tenis3D/Assets/Scripts/Bot.cs
@@ -11,11 +11,14 @@ public class Bot : MonoBehaviour
     Vector3 targetPosition;
     private const float Force = 8.5f;
 
+    public Transform[] targets;
 
+    ShotManager shotManager;
     void Start()
     {
         targetPosition = transform.position;
         animator = GetComponent<Animator>();
+        shotManager = GetComponent<ShotManager>();
     }
 
     void Update()
@@ -28,7 +31,20 @@ public class Bot : MonoBehaviour
         targetPosition.z = ball.position.z;
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
+    
+    Vector3 PickTarget()
+    {
+        int randomValue = UnityEngine.Random.Range(0, targets.Length-1);
+        return targets[randomValue].position;
+    }
 
+    Shot PickShot()
+    {
+        int randomValue = UnityEngine.Random.Range(0, 2);
+        if (randomValue == 0)
+            return shotManager.topSpin;
+        else return shotManager.flat;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
@@ -39,10 +55,12 @@ public class Bot : MonoBehaviour
 
     private void HitBall(Collider ballCollider)
     {
-        Vector3 hitDirection = aimTarget.position - transform.position;
+        Shot currentShot = PickShot();
+
+        Vector3 hitDirection = PickTarget() - transform.position;
         Rigidbody ballRigidbody = ballCollider.GetComponent<Rigidbody>();
 
-        ballRigidbody.linearVelocity = hitDirection.normalized * Force + new Vector3(0, 5, 0);
+        ballRigidbody.linearVelocity = hitDirection.normalized * currentShot.hitForce + new Vector3(0, currentShot.upForce, 0);
 
         PlayHitAnimation();
     }
